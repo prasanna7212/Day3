6280d42774e22cee2a044545256eb7dd09789cc1
prasanna7212 <81813282+prasanna7212@users.noreply.github.com>
2022-10-13 01:24:52 -0700

    Add files via upload

---
+++ PoistiveNegetative.java
@@ -0,0 +1,30 @@
+package Week1.Day3;
+
+public class PoistiveNegetative {
+
+	
+		// TODO Auto-generated method stub
+		public class PositiveNegative {
+			  public static void main(String[]args) {
+				int n = 9;
+				int i = -6;
+				if (n>=0) {
+					System.out.println("n : "+ "The number is Positive number");
+				}	
+				else {
+					System.out.println("n : "+ "The number is Negative number");
+				}
+				if (i>=0) {
+					System.out.println("i : " + "The number is Positive number");
+				}
+				else {
+					System.out.println("i : " + "The number is Negative number");
+					
+					}
+				
+			}
+			}
+			
+	}
+
+
